61c49787e330457694cb8d1a6ed10401151e678b
nanayaw <nybfam175@gmail.com>
2024-04-07 23:01:58 +0100

    my new bashscript

---
+++ firstseason.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo"#########################"
+echo "start of my first script"
+echo"#########################"
+mkdir devops
+cd devops
+ls 
+touch script.sh
+echo"#########################"
+echo " end of my first script"
+echo"#########################"
